b451684791cd0600648826a5452e285fc60adb1c
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2023-10-21 17:00:44 -0300

    Adição de comentários

--- hello-prisma/coletarNotas.ts
+++ hello-prisma/coletarNotas.ts
@@ -1,12 +1,14 @@
+// Script criado para a requisitar a nota de um grupo dada por professores ou alunos
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main(){
-  console.log(calcularNota("PROFESSOR","EXEMPLO"))//Printa a media das notas dadas pelos professores
+  console.log(calcularNota("PROFESSOR","EXEMPLO"))//Printa a media das notas dadas pelos professores para o grupo (substituir o exemplo pela id do grupo)
 }
 
 async function calcularNota(tipoUsuario: "ALUNO" | "PROFESSOR", grupoId: string) {
+  // Busca pelas avaliações do grupo feitas por um usuario com o tipo especificado
   const avaliacoes = await prisma.avaliacao.findMany({
     where: {
       grupoId,
@@ -24,7 +26,7 @@ async function calcularNota(tipoUsuario: "ALUNO" | "PROFESSOR", grupoId: string)
   if (totalAvaliacoes === 0) {
     return 0; // Evitar divisão por zero
   }
-
+  //Soma as notas para dividir pela quantidade de avaliações
   const somaNotas = avaliacoes.reduce((total, avaliacao) => {
     return total + avaliacao.nota;
   }, 0);
