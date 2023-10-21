e98c3868dfc3bf42682c894401b24513f69d0b70
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2023-10-21 17:02:17 -0300

    Adição de comentários

--- hello-prisma/avaliar.ts
+++ hello-prisma/avaliar.ts
@@ -1,12 +1,14 @@
+// Função para criar avaliacao
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 async function main(){
-    avaliar(10,'Exemplo','Exemplo')//Coloque valores que existam na database no lugar do Exemplo
+    avaliar(10,'Exemplo','Exemplo')//Coloque valores que existam na database no lugar do Exemplo , sendo as Id do grupo a receber avaliacao e a Id do usuario avaliando
 }
 
 async function avaliar(nota: number, usuarioId:string, grupoId:string) {
+    //Cria a avaliação
     const avalia = await prisma.avaliacao.create({
         data: {
             nota: nota,
